8f29b5eafdb7de7c57fa3735b1bd687ba7a66f7b
Blake Sakaluk <blake.sakaluk@gmail.com>
2015-10-16 18:39:54 -0600

    Lab 6 exercise

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -16,6 +16,7 @@ import junit.framework.TestCase;
 public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
 
     private String tweetText;
+    private String tweet2Text;
     private EditText bodyText;
     private Button saveButton;
 
@@ -33,6 +34,7 @@ public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 
         activity.getTweets().clear();
 
         tweetText = "Hello!";
+        tweet2Text = "Unhello!";
         bodyText = activity.getBodyText();
 
         activity.runOnUiThread(new Runnable() {
@@ -71,7 +73,7 @@ public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 
         getInstrumentation().waitForIdleSync();
 
         // Validate that ReceiverActivity is started
-        EditTweetActivity receiverActivity = (EditTweetActivity)
+        final EditTweetActivity receiverActivity = (EditTweetActivity)
                 receiverActivityMonitor.waitForActivityWithTimeout(1000);
         assertNotNull("ReceiverActivity is null", receiverActivity);
         assertEquals("Monitor for ReceiverActivity has not been called",
@@ -83,12 +85,23 @@ public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 
         getInstrumentation().removeMonitor(receiverActivityMonitor);
 
         // test that the tweet editor starts up with the correct tweet
+        receiverActivityBodyText = receiverActivity.getBodyText();
+        assertEquals(receiverActivityBodyText.getText(), newestTweet.getText());
+        receiverActivitySaveButton = receiverActivity.getSaveButton();
 
         // test that we can edit a tweet
-
         // test that we can push a save button for the edited tweet
+        receiverActivity.runOnUiThread(new Runnable() {
+            public void run() {
+                receiverActivityBodyText.setText(tweet2Text);
+                receiverActivitySaveButton.performClick();
+            }
+        });
+        getInstrumentation().waitForIdleSync();
 
         // test that the modified tweet was saved
+        newestTweet = (Tweet) oldTweetsList.getItemAtPosition(0);
+        assertEquals(tweet2Text, newestTweet.getText());
 
         // test that the modified tweet is in the tweet list
 
